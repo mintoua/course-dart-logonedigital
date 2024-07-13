f2304016e11b3e9bc7c421bf8bda90b622bd5fe7
mintoua <toupkandimintoua@gmail.com>
2024-07-13 18:57:15 +0100

    la declaration des variables et les types int, double, string, bool

--- main.dart
+++ main.dart
@@ -1,4 +1,13 @@
 void main(){
   print("Bienvenue dans la formation dev mobile avec flutter !!!");
-  print("Nous somme sur la branch Main");
+  print("Nous somme sur la branch Variables");
+
+  // Variables et les types basiques
+  String nomPlat = "Ndolé";
+  String descriptionPlat = "Plat traditionnel";
+  double prixPlat = 2560;
+  int idPlat = 2;
+  bool isDisponible;
+
+  print("Nom : " + nomPlat);
 }
