89fd83ee6dee5ffcf2b9667b526bea36fd1110d4
Fuhno17 <fugnoalexis@gmail.com>
2024-07-14 18:27:17 +0100

    mon second save

---
+++ perimetreducercle.dart
@@ -0,0 +1,19 @@
+void main(){
+     ///varible
+  const double pi = 3.1415;
+  int rayon = 10;
+  int h = 2;
+  double perimetre; 
+  perimetre = 2 * pi * rayon;
+  print (perimetre);
+
+  var age = 18;
+    if (age<20)
+    {
+      print("mineur");
+    }
+    else 
+    {
+      print("majeur");
+    }
+  }
