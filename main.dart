602773237120728e8d20d22fe3ab1984a9c9317c
mintoua <toupkandimintoua@gmail.com>
2024-07-27 19:05:05 +0100

    updated boucle

--- main.dart
+++ main.dart
@@ -1,4 +1,24 @@
 void main(){
   print("Bienvenue dans la formation dev mobile avec flutter !!!");
-  print("Nous somme sur la branch Main");
+  print("Nous somme sur la branch structures itératives");
+
+  //Boucle do  while
+  int i = 0;
+  do {
+    print("Itération $i");
+    i++;
+  } while (i < 5);
+
+  //Boucle for
+  for (int j = 0; j < 5; j++) {
+    print("Itération $j");
+  }
+  
+  //Boucle while
+  int k = 0;
+  while (k < 5) {
+    print("Itération $k");
+    k++;
+  }
+
 }
